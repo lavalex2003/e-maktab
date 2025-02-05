b2c3f8f34f94d099c553cd2bde6b40f47031cee0
Alexander S. <43718334+lavalex2003@users.noreply.github.com>
2025-02-05 15:39:02 +0500

    Update api.py

--- custom_components/emaktab/api.py
+++ custom_components/emaktab/api.py
@@ -1,26 +1,27 @@
-import requests
+import aiohttp
 import datetime
+import asyncio
 from .const import LOGIN_URL, SCHEDULE_URL
 
 class EMaktabAPI:
-    def __init__(self, username, password, person_id, school_id, group_id):
+    def __init__(self, username, password, person_id, school_id, group_id, session):
         self.username = username
         self.password = password
         self.person_id = person_id
         self.school_id = school_id
         self.group_id = group_id
-        self.session = requests.Session()
+        self.session = session
 
-    def login(self):
-        """Авторизация и получение cookies"""
-        response = self.session.post(LOGIN_URL, data={"username": self.username, "password": self.password, "captcha.id": "639dcede-f3e1-4e2f-8ef2-17f5f53aaca6", "exceededAttempts": "false"})
-        return response.status_code == 200  
+    async def login(self):
+        """Асинхронная авторизация"""
+        async with self.session.post(LOGIN_URL, data={"username": self.username, "password": self.password, "captcha.id": "639dcede-f3e1-4e2f-8ef2-17f5f53aaca6", "exceededAttempts": "false"}) as response:
+            return response.status == 200  
 
-    def get_schedule(self):
-        """Получение расписания на текущий день"""
+    async def get_schedule(self):
+        """Асинхронное получение расписания"""
         timestamp = int(datetime.datetime.combine(datetime.date.today(), datetime.time.min).timestamp())
         url = SCHEDULE_URL.format(self.person_id, self.school_id, self.group_id, timestamp)
-        response = self.session.get(url)
-        if response.status_code == 200:
-            return response.json()
-        return None
+        async with self.session.get(url) as response:
+            if response.status == 200:
+                return await response.json()
+            return None
