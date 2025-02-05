aba26a309645e48a5adbe2f87ad628ba117f315f
Alexander S. <43718334+lavalex2003@users.noreply.github.com>
2025-02-05 15:03:03 +0500

    Update const.py

--- custom_components/emaktab/const.py
+++ custom_components/emaktab/const.py
@@ -5,8 +5,7 @@ CONF_CAPTCHA = "639dcede-f3e1-4e2f-8ef2-17f5f53aaca6" #Идентификато�
 CONF_PERSON_ID = "person_id" #Идентификатор ученика, например: 1000009940640
 CONF_SCHOOL_ID = "school_id" #Идентификатор школы, например: 1000000000108
 CONF_GROUP_ID = "group_id" #Идентификатор группы учащихся, например: 2248292570190413489
-CONF_TAKEDAYS = 1 #Количество дней расписания
 UPDATE_INTERVAL = 3600  # Обновление раз в час
 BASE_URL = "https://emaktab.uz"
 LOGIN_URL = "https://login.emaktab.uz/"
-SCHEDULE_URL = BASE_URL + "/api/userfeed/persons/{}/schools/{}/groups/{}/schedule?date={}&takeDays=" + CONF_TAKEDAYS
+SCHEDULE_URL = BASE_URL + "/api/userfeed/persons/{}/schools/{}/groups/{}/schedule?date={}&takeDays=1"
